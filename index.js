825ae08d6503de84c941dd1b47aef5f76bfa31b4
Marcos <m.rodrig.felix@gmail.com>
2021-02-28 21:00:01 -0800

    commit tree 43d8b272f753723de9458cf785ec3b12ef6aa6bb

--- index.js
+++ index.js
@@ -1,5 +1,79 @@
 /* Your Code Here */
 
+function createEmployeeRecord(employeeInfo){
+    return {
+        firstName: employeeInfo[0],
+        familyName: employeeInfo[1],
+        title: employeeInfo[2],
+        payPerHour: employeeInfo[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+};
+
+
+function createEmployeeRecords(arrayOfArrays){
+    return arrayOfArrays.map( empArray => {
+        return createEmployeeRecord(empArray)
+    })
+};
+
+
+function createTimeInEvent(dateStamp){
+
+    const timeIn = {
+        type: "TimeIn",
+        hour: parseInt(dateStamp.split(' ')[1]),
+        date: dateStamp.split(' ')[0]
+    }
+
+    this.timeInEvents.push(timeIn)
+
+    return this
+};
+
+
+function createTimeOutEvent(dateStamp){
+
+    const timeOut = {
+        type: "TimeOut",
+        hour: parseInt(dateStamp.split(' ')[1]),
+        date: dateStamp.split(' ')[0]
+    }
+
+    this.timeOutEvents.push(timeOut)
+
+    return this
+};
+
+
+function hoursWorkedOnDate(workDate){
+   let dateIn = this.timeInEvents.find( e => {return e.date === workDate} )
+   let hourIn = dateIn.hour
+   let dateOut = this.timeOutEvents.find( e => {return e.date === workDate} )
+   let hourOut = dateOut.hour
+
+   return (hourOut - hourIn) / 100
+};
+
+
+function wagesEarnedOnDate(workDate){
+    const hoursWorked = hoursWorkedOnDate.call(this,workDate)
+    return hoursWorked * this.payPerHour
+};
+
+
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find( source => source.firstName === firstName )
+};
+
+
+function calculatePayroll(arrayOfEmployees){
+    return arrayOfEmployees.reduce( (total, employee) => {
+        return total + allWagesFor.call(employee)
+    }, 0)
+};  
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
